c7423923b7f8680e130bd9bdf94bc72d23e4ac9a
Eduardo Flores <46951897+bazinga183@users.noreply.github.com>
2023-01-26 17:00:00 -0500

    edit

--- reviews.py
+++ reviews.py
@@ -11,7 +11,7 @@ st.image('resources/APRO_Logo-A.png', width=100, output_format='PNG')
 st.title('RTO Glassdoor Reviews ')
 
 df = pd.read_excel(
-    io='bazinga183/employee-reviews/main/ALL GS Data.xlsx',
+    io='all_gs_data.xlsx',
     engine='openpyxl',
     skiprows=0,
     usecols='B:V',
